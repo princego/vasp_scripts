6a3d10c4c6ab66c4679a8c24a620a3990dd1544a
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2021-07-07 23:49:09 +0530

    Update multiple_jobs.sh

--- multiple_jobs.sh
+++ multiple_jobs.sh
@@ -3,6 +3,6 @@
 for i in job1 job2 job3 ; do # directories are named job1, job2, and job3. You can also use job* for multiple jobs 
     cd $i
     qsub ~/bin/vasp_job.sh # path to vasp submission script
-sleep 1 # sleep for 1 sec then submits next job
+sleep 1 # sleep for 1 sec, then submits next job
 done
 wait

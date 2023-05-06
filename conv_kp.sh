222045699b347878e14bd884e246aeda0dc7efac
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-05-07 01:51:45 +0300

    Update conv_kp.sh

--- conv_kp.sh
+++ conv_kp.sh
@@ -1,16 +1,16 @@
 ## kpoint convergence after setting ENCUT=550
 #!/bin/bash
-A='/home/prince/testing/bzo'
+A='/path/to/directory'
 ENCUT=550
 for ((  n=1;  n<=5;  n++  ))
 do
     KP=`head -$n $A/kpts | tail -1`
-    KP1=`head -$n $A/kpts1 | tail -1`
     mkdir $A/k_$KP
     cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR $A/8-1.sh $A/k_$KP
     sed -i "s/ENCUT = 500/ENCUT = $ENCUT/g" $A/k_$KP/INCAR
 
+#setting k-points
     cd $A/k_$KP
-    sed -i "4 s/.*/ $KP1/" KPOINTS
-    qsub 8-1.sh
+    KP1=`echo $KP | awk -F 'x' '{print $1" "$2" "$3}'`
+    sed -i "4 s/.*/ $KP1/g" KPOINTS
 done

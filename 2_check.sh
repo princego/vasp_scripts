ff4a5f37229dfac76ad92ed955d1f55d428c0e2e
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-05-06 22:14:16 +0300

    Create 2_check.sh

---
+++ 2_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+A='/path_to_directory'
+rm conv_en.csv
+for ((  n=1;  n<=5;  n++  ))
+do
+    EN=`head -$n $A/enrgs | tail -1`
+#check whether reached required accuracy
+    reached=$(grep "reached" $A/$EN/log)
+    echo "$EN: $reached"
+#checking total energy and energy per atom
+    totE=$(grep "free  energy   TOTEN" $A/$EN/OUTCAR|awk END{print}|awk '{print $5}')
+    nions=$(grep NIONS $A/$EN/OUTCAR | awk '{print $12}')
+    epa=$(echo "$totE / $nions" | bc -l)
+    echo "TOTEN:" $totE '|' "Energy_per_atom:" $epa
+    echo $EN,$totE,$epa >> conv_en
+done
+
+#calculating energy difference
+awk -F ',' 'NR==1{s=$3;next}{print $3-s;s=$3}' conv_en > conv_en_diff
+paste -d ',' conv_en conv_en_diff > conv_en.csv
+#adding header
+sed -i '1 i energy,toten,energy_per_atom,ediff_epa' conv_en.csv
+#remove temporary files
+rm conv_en conv_en_diff
+#display on screen
+cat conv_en.csv

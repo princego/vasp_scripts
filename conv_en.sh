8eb54e952ce0c6692af96ca6a77a368e4085bfd1
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-05-07 01:53:03 +0300

    Update conv_en.sh

--- conv_en.sh
+++ conv_en.sh
@@ -1,12 +1,13 @@
 ## Energy convergence (keep 5 energies in a seperate file named "enrgs")
+
 #!/bin/bash
-A='/path/to/directory'
+A='/home/prince/testing/bzo'
 for ((  n=1;  n<=5;  n++  ))
 do
     EN=`head -$n $A/enrgs | tail -1`
-    mkdir $A/$EN
-    cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR $A/8-1.sh $A/$EN
-    sed -i "s/ENCUT = 500/ENCUT = $EN/g" $A/$EN/INCAR
-    cd $A/$EN
+    mkdir $A/e_$EN
+    cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR $A/8-1.sh $A/e_$EN
+    sed -i "s/ENCUT = 500/ENCUT = $EN/g" $A/e_$EN/INCAR
+    cd $A/e_$EN
     qsub 8-1.sh
 done

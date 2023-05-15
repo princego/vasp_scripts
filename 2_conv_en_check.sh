21c983f8497aa6116f12861089764a64b7464634
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-05-15 16:18:54 +0300

    Update and rename conv_en_check.sh to 2_conv_en_check.sh

--- 2_conv_en_check.sh
+++ 2_conv_en_check.sh
@@ -1,19 +1,18 @@
-## check whether energy is converged
 #!/bin/bash
-A='/path/to/directory'
+A='/path/to/file'
 rm conv_en.csv
 for ((  n=1;  n<=6;  n++  ))
 do
     EN=`head -$n $A/enrgs | tail -1`
 #check whether reached required accuracy
-    reached=$(grep "reached" $A/e_$EN/log)
-    echo "$EN: $reached"
+        reached=$(grep "reached" $A/e_$EN/log)
+        echo "$EN: $reached"
 #checking total energy and energy per atom
-    totE=$(grep "free  energy   TOTEN" $A/e_$EN/OUTCAR|awk END{print}|awk '{print $5}')
-    nions=$(grep NIONS $A/e_$EN/OUTCAR | awk '{print $12}')
-    epa=$(echo "$totE / $nions" | bc -l)
-    echo "TOTEN:" $totE '|' "Energy_per_atom:" $epa
-    echo $EN,$totE,$epa >> conv_en
+        totE=$(grep "free  energy   TOTEN" $A/e_$EN/OUTCAR|awk END{print}|awk '{print $5}')
+        nions=$(grep NIONS $A/e_$EN/OUTCAR | awk '{print $12}')
+        epa=$(echo "$totE / $nions" | bc -l)
+        echo "TOTEN:" $totE '|' "Energy_per_atom:" $epa
+        echo $EN,$totE,$epa >> conv_en
 done
 
 #calculating energy difference

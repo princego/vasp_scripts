2d8d2586dd392b8178c575060bf8a9d9c2652f47
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-05-15 16:16:05 +0300

    Update and rename conv_kp.sh to 3_conv_kp.sh

---
+++ 3_conv_kp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+A='/home/princeg/fuel_cell/BaZrO3/u_BaZrO3-5'
+ENCUT=700
+for ((  n=1;  n<=9;  n++  ))
+do
+        KP=`head -$n $A/kpts | tail -1`
+        mkdir $A/k_$KP
+        cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR $A/job.sh $A/k_$KP
+        sed -i "s/ENCUT = 500/ENCUT = $ENCUT/g" $A/k_$KP/INCAR
+
+#setting k-points
+        cd $A/k_$KP
+        KP1=`echo $KP | awk -F 'x' '{print $1" "$2" "$3}'`
+        sed -i "4 s/.*/ $KP1/g" KPOINTS
+        qsub job.sh
+done

b95cd677772780b531ab7146f12ca2e52b462ea1
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-07-25 14:55:54 +0300

    Update 3_conv_kp.sh

--- convergence_test/3_conv_kp.sh
+++ convergence_test/3_conv_kp.sh
@@ -6,13 +6,13 @@ for ((  n=1;  n<=10;  n++  ))
 do
         KP=`head -$n $A/kpts | tail -1`
         mkdir $A/k_$KP
-        cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR  $A/k_$KP
+        cp $A/INCAR $A/KPOINTS $A/POSCAR $A/POTCAR $A/job.sh $A/k_$KP
 # replacing the ENCUT value with our converged value
         sed -i "/ENCUT/c\   ENCUT  = $ENCUT" $A/k_$KP/INCAR
 # setting k-points
         cd $A/k_$KP
         KP1=`echo $KP | awk -F 'x' '{print $1" "$2" "$3}'`
         sed -i "4 s/.*/ $KP1/g" KPOINTS
-        qsub ~/bin/8-2.sh
+        qsub job.sh
         cd $A
 done

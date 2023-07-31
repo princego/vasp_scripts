f1b5bf14819ded26e3c3c2f9b7f20d2608f5a0ab
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-07-31 16:15:33 +0300

    Update head_tail_split.sh

--- head_tail_split.sh
+++ head_tail_split.sh
@@ -1,6 +1,6 @@
 # Script to seperate head, and  tail consisting of atomic positions in Cartesian of a vasp file
 #Enter the filename you want to split
-FILENAME="Al2O3_avg_latt_par.vasp"
+FILENAME="POSCAR"
 
 #grep a line before "Cartesian" to count the num of atoms later on
 LINE=$(cat $FILENAME | grep -B 1 Cartesian | head -1)
@@ -10,8 +10,8 @@ echo atoms: $LINE
 NUMATOMS=$(echo $LINE | tr ' ' '\n' | awk '{sum+=$1} END {print sum}')
 echo total atoms: $NUMATOMS
 
-#prints all atoms after "Cartesian" to a file named tail
-grep -A $NUMATOMS Cartesian $FILENAME | tail -$NUMATOMS > tail
-
 #print the tags  before (including) "Cartesian" to a file name head
-grep -B 7 Cartesian $FILENAME > head
+grep -B 7 Cartesian $FILENAME > head_$FILENAME
+
+#prints all atoms after "Cartesian" to a file named tail
+grep -A $NUMATOMS Cartesian $FILENAME | tail -$NUMATOMS > tail_$FILENAME
